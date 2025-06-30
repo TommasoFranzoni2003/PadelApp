b39b34a1ac830b6e78b47c941bccf8a43e1dbd2c
TommasoFranzoni2003 <tommasofranzoni10@gmail.com>
2025-06-30 14:47:38 +0200

    Third Test Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<!-- PROGETTO DI PROGRAMMAZIONE WEB -->
         <title>PadelApp</title>
 
         <!-- Fonts -->
