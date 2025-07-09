de088d700f3e2e0271855b0477900fdc7c77d385
alebelussi <alexbelussi@gmail.com>
2025-07-09 15:57:43 +0200

    ProfileImage added

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,60 +1,80 @@
+<!-- NAVBAR -->
 <nav id="mainNav" class="navbar navbar-expand-lg navbar-dark bg-transparent custom-navbar">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{ route('homepage') }}">
+    <a class="navbar-brand" href="{{ route('homepage') }}"> <!-- LOGO -->
         <img src="{{ asset('image/logo.png') }}" alt="Logo" class="img-fluid">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent"> <!-- CONTENITORE MENU -->
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center">
-        {{-- Dropdown Campi --}}
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="campiDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Campi </a>
-          <ul class="dropdown-menu" aria-labelledby="campiDropdown">
-            @auth
-              @if (Auth::user()->hasRole('admin'))
-                <li><a class="dropdown-item" href="{{ route('court.store') }}">Inserisci Campo</a></li>
-              @endif
-            @endauth
-            <li><a class="dropdown-item" href="{{ route('court.show') }}">Visualizza Campi</a></li>
-          </ul>
-        </li>
 
-        {{-- Dropdown Strutture --}}
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="struttureDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Strutture </a>
-          <ul class="dropdown-menu" aria-labelledby="struttureDropdown">
-            
-            @auth
-              @if (Auth::user()->hasRole('admin'))
-                <li><a class="dropdown-item" href="{{ route('complex.store') }}">Inserisci Struttura</a></li>
-              @endif
-            @endauth
-
-            <li><a class="dropdown-item" href="{{ route('complex.show') }}">Visualizza Struttura</a></li>
-          </ul>
-        </li>
-
-        {{-- Dropdown Prenotazioni --}}
+        {{-- Campi --}}
         @auth
-          @if (Auth::user()->hasAnyRole(['admin', 'user']))
+          @if (Auth::user()->hasRole('admin'))
+            <li class="nav-item dropdown">  <!-- CAMPI -->
+              <a class="nav-link dropdown-toggle" href="#" id="campiDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Campi</a>
+              <ul class="dropdown-menu" aria-labelledby="campiDropdown">
+                <li><a class="dropdown-item" href="{{ route('court.store') }}">Inserisci</a></li>
+                <li><a class="dropdown-item" href="{{ route('court.show') }}">Visualizza</a></li>
+              </ul>
+            </li>
+          @else 
+            {{-- Utente non admin: link singolo senza dropdown --}}
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('court.show') }}">Campi</a>
+            </li>
+          @endif
+        @else
+          {{-- Non autenticato, mostra solo visualizza --}}
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('court.show') }}">Campi</a>
+          </li>
+        @endauth
+
+        {{-- Strutture --}}
+        @auth
+          @if (Auth::user()->hasRole('admin'))
+            <li class="nav-item dropdown">  <!-- STRUTTURE -->
+              <a class="nav-link dropdown-toggle" href="#" id="struttureDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Strutture</a>
+              <ul class="dropdown-menu" aria-labelledby="struttureDropdown">
+                <li><a class="dropdown-item" href="{{ route('complex.store') }}">Inserisci</a></li>
+                <li><a class="dropdown-item" href="{{ route('complex.show') }}">Visualizza</a></li>
+              </ul>
+            </li>
+          @else
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('complex.show') }}">Strutture</a>
+            </li>
+          @endif
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('complex.show') }}">Strutture</a>
+          </li>
+        @endauth
+
+        {{-- Prenotazioni --}}
+        @auth
+          @if (Auth::user()->hasRole('admin'))  <!-- PRENOTAZIONI -->
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="prenotazioniDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Prenotazioni
-              </a>
+              <a class="nav-link dropdown-toggle" href="#" id="prenotazioniDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Prenotazioni</a>
               <ul class="dropdown-menu" aria-labelledby="prenotazioniDropdown">
                 <li><a class="dropdown-item" href="{{ route('booking.add') }}">Iscrizione</a></li>
                 <li><a class="dropdown-item" href="{{ route('booking.show') }}">Visualizza</a></li>
               </ul>
             </li>
+          @elseif (Auth::user()->hasRole('user'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('booking.show') }}">Prenotazioni</a>
+            </li>
           @endif
         @endauth
 
         {{-- Link per ospiti --}}
-        @guest
-          <li class="nav-item">
+        @guest    
+          <li class="nav-item">   <!-- PROFILO NON LOGGGATO -->
             <a class="nav-link" href="{{ route('login') }}">Login</a>
           </li>
           <li class="nav-item">
@@ -64,14 +84,12 @@
 
         {{-- Dropdown utente loggato --}}
         @auth
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="{{ Auth::user()->profile_photo_path ? asset('storage/' . Auth::user()->profile_photo_path) : asset('image/default-profile.png') }}"
-                  alt="Immagine Profilo"
-                  class="profile-image">
-            </a>
+          <li class="nav-item dropdown">  <!-- PROFILO LOGGATO -->
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <x-avatar class="profile-image" :user="Auth::user()" />
+          </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-              <li><a class="dropdown-item" href="{{ route('profile.show') }}">Profilo</a>
+              <li><a class="dropdown-item" href="{{ route('profile.show') }}">Profilo</a></li>
               <li>
                 <form method="POST" action="{{ route('logout') }}">
                   @csrf
@@ -81,7 +99,9 @@
             </ul>
           </li>
         @endauth
+
       </ul>
+
     </div>
   </div>
 </nav>
